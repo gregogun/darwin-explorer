7cf9b76a157e246b61054c123291f1eb90010365
gregogun <gregogun97@gmail.com>
2023-05-22 17:29:01 +0100

    chore: resuse functions

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,30 @@
 import { formatDistance } from "date-fns";
 
 export const timeAgo = (date: number) => {
-  return formatDistance(new Date(date), new Date(), { addSuffix: true });
+  return formatDistance(new Date(date), new Date(), {
+    addSuffix: true,
+  }).replace("about", "");
+};
+
+interface AbbreviateAddressOptions {
+  startChars?: number;
+  endChars?: number;
+  noOfEllipsis?: number;
+}
+
+interface AbbreviateAddress {
+  address: string | undefined;
+  options?: AbbreviateAddressOptions;
+}
+
+export const abbreviateAddress = ({
+  address,
+  options = {},
+}: AbbreviateAddress) => {
+  const { startChars = 5, endChars = 4, noOfEllipsis = 2 } = options;
+
+  const dot = ".";
+  const firstFive = address?.substring(0, startChars);
+  const lastFour = address?.substring(address.length - endChars);
+  return `${firstFive}${dot.repeat(noOfEllipsis)}${lastFour}`;
 };
