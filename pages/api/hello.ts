aa9e840dd0119b9241777a9c88134be829adca7a
gregogun <gregogun97@gmail.com>
2023-05-22 19:50:36 +0100

    fix: remove getAsset import from api route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getAsset } from "../../lib/asset-sdk.ts/index.js";
 
 export default async function handler(
   req: NextApiRequest,
