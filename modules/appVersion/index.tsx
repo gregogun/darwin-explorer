74dc98b42e24d4fe77d8d466d19b522710b3f004
gregogun <gregogun97@gmail.com>
2023-05-26 09:47:06 +0100

    feat: update version info query

--- modules/appVersion/index.tsx
+++ modules/appVersion/index.tsx
@@ -20,6 +20,8 @@ import { useLocation } from "react-router-dom";
 import { getVersionInfo } from "../../lib/getVersionInfo";
 import { Skeleton } from "../../ui/Skeleton";
 import { abbreviateAddress, timeAgo } from "../../utils";
+import { getStampCount, stampAsset } from "../../lib/stamps";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 
 interface VersionProps {
   title: string;
@@ -37,6 +39,22 @@ const AppVersion = () => {
   const [version, setVersion] = useState<VersionProps>();
   const [isCopied, setIsCopied] = useState(false);
   const location = useLocation();
+  const queryClient = useQueryClient();
+  const {
+    data: stamps,
+    isLoading: stampsLoading,
+    isError: stampsError,
+  } = useQuery({
+    queryKey: ["stamps"],
+    enabled: !!version?.txid,
+    queryFn: () => {
+      if (!version?.txid) {
+        throw new Error("No txid for the asset provided");
+      }
+
+      return getStampCount(version.txid);
+    },
+  });
 
   useEffect(() => {
     const query = location.search;
@@ -72,232 +90,255 @@ const AppVersion = () => {
     });
   };
 
+  const mutation = useMutation({
+    mutationFn: stampAsset,
+    onSuccess: (data) => {
+      console.log(data);
+      queryClient.invalidateQueries({ queryKey: ["stamps"] });
+    },
+    onError: (error: any) => {
+      console.error(error);
+    },
+  });
+
+  const handleClick = () => {
+    if (!version?.txid) {
+      return;
+    }
+
+    mutation.mutate(version?.txid);
+  };
+
   return (
-    <>
-      {/* <AppHeader /> */}
-      <Flex
-        direction="column"
-        css={{
-          mt: "$10",
-          maxW: 680,
-          mx: "auto",
-        }}
-      >
-        <Box>
-          <Flex justify="between">
-            {version ? (
-              <Typography
-                as="h2"
+    <Flex
+      direction="column"
+      css={{
+        mt: "$10",
+        maxW: 680,
+        mx: "auto",
+      }}
+    >
+      <Box>
+        <Flex justify="between">
+          {version ? (
+            <Typography
+              as="h2"
+              css={{
+                color: "$slate12",
+              }}
+              size="8"
+              weight="6"
+            >
+              {version.title}
+            </Typography>
+          ) : (
+            <Skeleton
+              css={{
+                br: "$1",
+                width: 200,
+                height: "$7",
+              }}
+            />
+          )}
+          <Flex gap="5">
+            <Button
+              disabled={!version}
+              as="a"
+              href={`https://g8way.io/${version?.txid}`}
+              colorScheme="indigo"
+              css={{ gap: "$2" }}
+            >
+              Visit
+              <ArrowRightIcon />
+            </Button>
+            <Button
+              onClick={handleClick}
+              disabled={!version}
+              colorScheme="indigo"
+              variant="solid"
+            >
+              <HiThumbUp />
+              Stamp
+              <Flex
                 css={{
-                  color: "$slate12",
+                  backgroundColor: "$indigo11",
+                  [`.${darkTheme} &`]: {
+                    backgroundColor: "$indigo8",
+                  },
+                  pt: 2,
+                  px: "$1",
+                  placeItems: "center",
+                  minWidth: 20,
+                  maxHeight: 20,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  br: "$pill",
+                  fontSize: 10,
+                  color: "white",
                 }}
-                size="8"
-                weight="6"
               >
-                {version.title}
+                {stampsLoading || stampsError || typeof stamps === "undefined"
+                  ? "-"
+                  : stamps}
+              </Flex>
+            </Button>
+          </Flex>
+        </Flex>
+        <Flex
+          css={{
+            mt: "$1",
+            "& p": {
+              color: "$slate11",
+              fontSize: "$2",
+              lineHeight: "$2",
+            },
+          }}
+          align="center"
+          gap="1"
+        >
+          {version ? (
+            <>
+              <Typography>
+                Created by {abbreviateAddress({ address: version.owner })}
               </Typography>
-            ) : (
-              <Skeleton
-                css={{
-                  br: "$1",
-                  width: 200,
-                  height: "$7",
-                }}
-              />
-            )}
-            <Flex gap="5">
-              <Button
-                disabled={!version}
-                as="a"
-                href={`https://g8way.io/${version?.txid}`}
-                colorScheme="indigo"
-                css={{ gap: "$2" }}
-              >
-                Visit
-                <ArrowRightIcon />
-              </Button>
-              <Button disabled={!version} colorScheme="indigo" variant="solid">
-                <HiThumbUp />
-                Stamp
-                <Flex
-                  css={{
-                    backgroundColor: "$indigo11",
-                    [`.${darkTheme} &`]: {
-                      backgroundColor: "$indigo8",
-                    },
-                    pt: 2,
-                    px: "$1",
-                    placeItems: "center",
-                    minWidth: 20,
-                    maxHeight: 20,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    br: "$pill",
-                    fontSize: 10,
-                    color: "white",
-                  }}
-                >
-                  1
-                </Flex>
-              </Button>
-            </Flex>
-          </Flex>
-          <Flex
+              <Typography>•</Typography>
+              <Typography>{timeAgo(Number(version.published))}</Typography>
+            </>
+          ) : (
+            <Skeleton
+              css={{
+                br: "$1",
+                width: 240,
+                height: "$5",
+              }}
+            />
+          )}
+        </Flex>
+        <Flex css={{ mt: "$10" }} direction="column" gap="1">
+          <Typography css={{ color: "$slate11" }}>Description</Typography>
+          {version ? (
+            <Typography>{version.description}</Typography>
+          ) : (
+            <Skeleton
+              css={{
+                br: "$1",
+                width: "100%",
+                height: "$30",
+              }}
+            />
+          )}
+        </Flex>
+        <Flex css={{ mt: "$10" }} gap="5">
+          <Button
+            variant="ghost"
             css={{
-              mt: "$1",
-              "& p": {
-                color: "$slate11",
-                fontSize: "$2",
-                lineHeight: "$2",
+              gap: "$2",
+              p: 0,
+              "&:hover": {
+                backgroundColor: "transparent",
+                color: "$slate12",
               },
+              "&:active": { backgroundColor: "transparent" },
             }}
-            align="center"
-            gap="1"
+            disabled={!version}
+            href={`https://g8way.io/${version?.metaId}`}
+            as="a"
           >
-            {version ? (
-              <>
-                <Typography>
-                  Created by {abbreviateAddress({ address: version.owner })}
-                </Typography>
-                <Typography>•</Typography>
-                <Typography>{timeAgo(Number(version.published))}</Typography>
-              </>
-            ) : (
-              <Skeleton
-                css={{
-                  br: "$1",
-                  width: 240,
-                  height: "$5",
-                }}
-              />
-            )}
-          </Flex>
-          <Flex css={{ mt: "$10" }} direction="column" gap="1">
-            <Typography css={{ color: "$slate11" }}>Description</Typography>
-            {version ? (
-              <Typography>{version.description}</Typography>
-            ) : (
-              <Skeleton
-                css={{
-                  br: "$1",
-                  width: "100%",
-                  height: "$30",
-                }}
-              />
-            )}
-          </Flex>
-          <Flex css={{ mt: "$10" }} gap="5">
-            <Button
-              variant="ghost"
-              css={{
-                gap: "$2",
-                p: 0,
-                "&:hover": {
-                  backgroundColor: "transparent",
-                  color: "$slate12",
-                },
-                "&:active": { backgroundColor: "transparent" },
-              }}
-              disabled={!version}
-              href={`https://g8way.io/${version?.metaId}`}
-              as="a"
-            >
-              Release Notes
-              <FileTextIcon />
-            </Button>
-            <Button
-              variant="ghost"
-              css={{
-                gap: "$2",
-                p: 0,
-                "&:hover": {
-                  backgroundColor: "transparent",
-                  color: "$slate12",
-                },
-                "&:active": { backgroundColor: "transparent" },
-              }}
-              disabled={!version}
-              href={`https://g8way.io/${version?.source}`}
-              as="a"
-            >
-              Download source
-              <DownloadIcon />
-            </Button>
-            <Button
-              variant="ghost"
-              css={{
-                gap: "$2",
-                p: 0,
-                pointerEvents: isCopied ? "none" : "auto",
-                color: isCopied ? "$green11" : "$slate11",
-                "&:hover": {
-                  backgroundColor: "transparent",
-                  color: "$slate12",
-                },
-                "&:active": { backgroundColor: "transparent" },
-              }}
-              disabled={!version}
-              onClick={() => handleCopy(version?.txid)}
-            >
-              {isCopied ? "Copied!" : "Copy Version ID"}
-              {isCopied ? <CheckIcon /> : <CopyIcon />}
-            </Button>
-          </Flex>
-          <Box
+            Release Notes
+            <FileTextIcon />
+          </Button>
+          <Button
+            variant="ghost"
             css={{
-              background:
-                "linear-gradient(89.46deg, #1A1B1E 1.67%, rgba(26, 29, 30, 0) 89.89%)",
-              height: 1,
-              my: "$3",
+              gap: "$2",
+              p: 0,
+              "&:hover": {
+                backgroundColor: "transparent",
+                color: "$slate12",
+              },
+              "&:active": { backgroundColor: "transparent" },
             }}
-          />
-          <Flex
+            disabled={!version}
+            href={`https://g8way.io/${version?.source}`}
+            as="a"
+          >
+            Download source
+            <DownloadIcon />
+          </Button>
+          <Button
+            variant="ghost"
             css={{
-              mt: "$5",
-              p: "$3",
-              boxShadow: "0 0 0 1px $colors$slate3",
-              br: "$3",
+              gap: "$2",
+              p: 0,
+              pointerEvents: isCopied ? "none" : "auto",
+              color: isCopied ? "$green11" : "$slate11",
+              "&:hover": {
+                backgroundColor: "transparent",
+                color: "$slate12",
+              },
+              "&:active": { backgroundColor: "transparent" },
+            }}
+            disabled={!version}
+            onClick={() => handleCopy(version?.txid)}
+          >
+            {isCopied ? "Copied!" : "Copy Version ID"}
+            {isCopied ? <CheckIcon /> : <CopyIcon />}
+          </Button>
+        </Flex>
+        <Box
+          css={{
+            background:
+              "linear-gradient(89.46deg, #1A1B1E 1.67%, rgba(26, 29, 30, 0) 89.89%)",
+            height: 1,
+            my: "$3",
+          }}
+        />
+        <Flex
+          css={{
+            mt: "$5",
+            p: "$3",
+            boxShadow: "0 0 0 1px $colors$slate3",
+            br: "$3",
+
+            "&:hover": {
+              boxShadow: "0 0 0 1px $colors$slate4",
+            },
+
+            "&:focus-within": {
+              boxShadow: "0 0 0 2px $colors$blue8",
+            },
+          }}
+          direction="column"
+          gap="5"
+        >
+          <Textarea
+            css={{
+              boxShadow: "none",
+              minHeight: 80,
+              resize: "none",
 
               "&:hover": {
-                boxShadow: "0 0 0 1px $colors$slate4",
+                boxShadow: "none",
               },
 
-              "&:focus-within": {
-                boxShadow: "0 0 0 2px $colors$blue8",
+              "&:focus": {
+                boxShadow: "none",
               },
             }}
-            direction="column"
-            gap="5"
+            maxLength={300}
+            variant="outline"
+            placeholder="Share your thoughts..."
+          />
+          <Button
+            css={{ alignSelf: "end" }}
+            variant="solid"
+            colorScheme="indigo"
           >
-            <Textarea
-              css={{
-                boxShadow: "none",
-                minHeight: 80,
-                resize: "none",
-
-                "&:hover": {
-                  boxShadow: "none",
-                },
-
-                "&:focus": {
-                  boxShadow: "none",
-                },
-              }}
-              maxLength={300}
-              variant="outline"
-              placeholder="Share your thoughts..."
-            />
-            <Button
-              css={{ alignSelf: "end" }}
-              variant="solid"
-              colorScheme="indigo"
-            >
-              Comment
-            </Button>
-          </Flex>
-        </Box>
-      </Flex>
-    </>
+            Comment
+          </Button>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
